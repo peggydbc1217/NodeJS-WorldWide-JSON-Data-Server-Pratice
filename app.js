8c2918ada356bfba77549f162d1e194c2a55467d
Larry <peggydbc1217@gmail.com>
2023-07-27 15:24:48 +0800

    seconed

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const worldwideRouter = require("./worldwideRouter");
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/", worldwideRouter);
 
 //如果上面的router都沒有match到，就會進到這個middleware handle錯誤
@@ -14,6 +15,6 @@ app.all("*", (req, res, next) => {
   });
 });
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`App running `);
 });
